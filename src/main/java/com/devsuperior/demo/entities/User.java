a5d604c8ff438c35c5de93e279b225fb8fa261f2
icaro-freitas <icaro_freitas2014@outlook.com>
2024-09-17 22:00:16 -0300

    Database seeding created

--- src/main/java/com/devsuperior/demo/entities/User.java
+++ src/main/java/com/devsuperior/demo/entities/User.java
@@ -27,8 +27,7 @@ public class User implements UserDetails {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String firstName;
-	private String lastName;
+	private String name;
 	@Column(unique = true)
 	private String email;
 	private String password;
@@ -40,10 +39,9 @@ public class User implements UserDetails {
 	public User() {
 	}
 
-	public User(Long id, String firstName, String lastName, String email, String password) {
+	public User(Long id, String name, String email, String password) {
 		this.id = id;
-		this.firstName = firstName;
-		this.lastName = lastName;
+		this.name = name;
 		this.email = email;
 		this.password = password;
 	}
@@ -56,20 +54,12 @@ public class User implements UserDetails {
 		this.id = id;
 	}
 
-	public String getFirstName() {
-		return firstName;
+	public String getName() {
+		return this.name;
 	}
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
+	public void setName(String name) {
+		this.name = name;
 	}
 
 	public String getEmail() {
